2ab1c2939237b1d2b0676ae262a524da5f93fae5
aryannP03 <arpatel@trueigtech.com>
2026-02-20 15:32:03 +0530

    feat: add debounce to search products

--- src/components/product-page/hooks/useProductsearch.js
+++ src/components/product-page/hooks/useProductsearch.js
@@ -1,19 +1,22 @@
 'use client'
 import { useState, useMemo } from "react"
+import useDebounce from "@/hooks/useDebounce"
 
 function useProductSearch(products = []) {
   const [searchTerm, setSearchTerm] = useState("")
   const [category, setCategory] = useState("All")
 
+  const debouncedSearch = useDebounce(searchTerm, 2000, 3)
+
   const filteredProducts = useMemo(() => {
     return products.filter((product) => {
-      const Search = product.title.toLowerCase().includes(searchTerm.toLowerCase())
+      const Search = product.title.toLowerCase().includes(debouncedSearch.toLowerCase())
 
       const selectCategory = category === "All" || product.category.toLowerCase() === category.toLowerCase()
 
       return selectCategory && Search
     })
-  }, [products, searchTerm, category])
+  }, [products, debouncedSearch, category])
 
   return {
     filteredProducts,
